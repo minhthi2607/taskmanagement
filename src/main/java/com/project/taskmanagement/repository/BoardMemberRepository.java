118009275142ba145b9873909781378d9bccfb2c
nvthanh-2003 <thanh2k3nb@gmail.com>
2026-08-12 21:34:39 +0700

    fix(merge): restore missing methods in BoardMemberRepository

--- src/main/java/com/project/taskmanagement/repository/BoardMemberRepository.java
+++ src/main/java/com/project/taskmanagement/repository/BoardMemberRepository.java
@@ -1,6 +1,7 @@
 package com.project.taskmanagement.repository;
 
 import com.project.taskmanagement.entity.BoardMember;
+import com.project.taskmanagement.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,14 @@ import java.util.Optional;
 public interface BoardMemberRepository extends JpaRepository<BoardMember, Long> {
 
     boolean existsByBoardIdAndUserId(Long boardId, Long userId);
+
+    boolean existsByBoardIdAndUserIdAndRole(Long boardId, Long userId, Role role);
+
+    Optional<BoardMember> findByBoardIdAndUserId(Long boardId, Long userId);
+
+    List<BoardMember> findByBoardId(Long boardId);
+
+    List<BoardMember> findByUserId(Long userId);
+
+    void deleteByBoardIdAndUserId(Long boardId, Long userId);
 }
